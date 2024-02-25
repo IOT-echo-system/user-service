162a02f502f53ce1da56cba566f522c54d20bd1e
Shivam Rajput <shivam.rajput@thoughtworks.com>
2024-02-25 10:29:52 +0530

    Shivi | Add gson as dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation(files("./libs/logging-starter-0.0.1.jar"))
     implementation(files("./libs/iot-utils-starter-0.0.1.jar"))
     implementation(files("./libs/mqtt-starter-0.0.1.jar"))
+    implementation ("com.google.code.gson:gson:2.8.8")
     implementation("org.springframework.integration:spring-integration-mqtt:6.0.0")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
